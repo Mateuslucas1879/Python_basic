650b8b0a992e8c78b519d5488128da3bef4ee096
Mateus Lucas <mateuslucas1879@gmail.com>
2024-03-05 00:41:47 -0300

    Atualiza Intermediario

--- INTERMEDIARIO/Clouser.py
+++ INTERMEDIARIO/Clouser.py
@@ -1,1 +1,11 @@
-J
+def criar_saudacao(saudacao,nome):
+    def saudar():
+        return f'{saudacao},{nome}'
+    return saudar
+
+
+s1 = criar_saudacao('Bom Dia','Luana!!!')
+s2 = criar_saudacao('Bom Dia','Yasmin!!!')
+
+print(s1())
+print(s2())
